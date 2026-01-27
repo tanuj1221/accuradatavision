55bd8774b80315f6479facf065363c5d6b4cd45f
tanuj1221 <tcchavan999@gmail.com>
2026-01-27 11:01:07 +0530

    fixing routes

--- backend/server.js
+++ backend/server.js
@@ -207,7 +207,7 @@ const path = require('path');
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
 // Catch-all handler for any request that doesn't match an API route
-app.get('*', (req, res) => {
+app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
 });
 
